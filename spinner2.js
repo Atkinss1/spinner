46343737e2afef395f08d8880f0f43b09a435cc0
Atkins <100738958+Atkinss1@users.noreply.github.com>
2023-10-14 22:52:07 -0600

    Update spinner2.js
    add time variable

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,10 @@
 // refactoring my example spinner that uses the return character ('/r') to create an animated spinner
 
 const spinner = '\r|\r/\r-\r\\\r|\r-';
-
+let time = 5000;
 for (const char of spinner) {
   setTimeout(() => {
     process.stdout.write(char);
-  }, 100);
+  }, time);
+  time * 0.2;
 }
